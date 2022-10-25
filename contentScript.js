a1d5f2ee2c50a7143ac0362014d978d3cd3b1c87
nephydecode <nephydecode@gmail.com>
2022-10-26 00:54:40 +0800

    refactor: remove whitespaces & unused variables

--- contentScript.js
+++ contentScript.js
@@ -12,44 +12,39 @@ const pages = {
     Sidequest : "Sidequest"
 }
 
-let currentPage;
-
 document.addEventListener('keydown', (event) => {
-    var name = event.key;
     var code = event.code;
-    
     if(code==="ShiftLeft") autoNext();
-
   }, false);
   
 function autoNext () {
-        checkPage().then((e)=> {
-            console.log(e);
-            switch(e){
+    checkPage().then((e)=> {
+        console.log(e);
+        switch(e){
             case "Start":
                 const answer = addbits(document.querySelectorAll("label")[6].innerText.slice(27,-3).replaceAll(' ',''))
                 document.getElementById('nojs-solve-v').value = answer
                 document.getElementsByTagName("form")[0].submit()
-                case "Select" :
-                    document.getElementsByTagName("form")[0].submit()
-                    break
-                case "Battle" :
-                    document.getElementsByTagName("form")[1].submit()
-                    break
-                case "Next":
-                    document.getElementsByClassName("menu-tab")[0].click()
-                    break
+            case "Select" :
+                document.getElementsByTagName("form")[0].submit()
+                break
+            case "Battle" :
+                document.getElementsByTagName("form")[1].submit()
+                break
+            case "Next":
+                document.getElementsByClassName("menu-tab")[0].click()
+                break
             case "Error" :
                 window.location.href = 'https://pokemon-vortex.com/side-quest'
                 break
             case "Sidequest" :
                 document.getElementsByClassName('button-maroon button-large margin-bottom-15 margin-top-10')[0].click()
                 break
-                default:
-                    break
-            }
-        });
-    }
+            default:
+                break
+        }
+    });
+}
 
 async function checkPage() {
 
@@ -64,7 +59,6 @@ async function checkPage() {
 
     // COMPLETE BATTLE PAGE
     if (document.getElementsByClassName("heading-maroon no-bot-border-rad margin-bottom-3")[0]!== undefined && document.getElementsByClassName("heading-maroon no-bot-border-rad margin-bottom-3")[0].innerHTML === "Congratulations! You won the battle!") { 
-        currentPage = "Next";
         return pages.Next;
     }
 
@@ -82,19 +76,18 @@ async function checkPage() {
     // BATTLE PAGE
     const battle = document.getElementsByClassName("heading-maroon no-right-border-rad margin-right-2");
     if (battle[0] !== undefined && battle[0].innerHTML == "Select an Attack" || battle !== undefined && battle[0].innerHTML === "Attack Results") {
-        currentPage = "Battle";
         return pages.Battle;
     }
 
-    }
+}
 
 // Simple Equation Solver with only + and - operations
 function addbits(s) {
     var total = 0,
         s = s.match(/[+\-]*(\.\d+|\d+(\.\d+)?)/g) || [];
-
+        
     while (s.length) {
       total += parseFloat(s.shift());
     }
     return total;
-}
+  }
